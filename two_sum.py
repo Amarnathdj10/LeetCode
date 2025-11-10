9367b4aa3278ac27afa23685a9d0a4654b7041a7
Amarnathdj10 <amarnathdj56@gmail.com>
2025-11-10 20:40:16 +0530

    deleted

--- two_sum.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(range(nums))):
-                if nums[i]+nums[j] == target:
-                    return list(nums[i],nums[j])
