3a9de6cb7d2669e5a04370f4b9c92ccc3bfd390b
Amarnathdj10 <amarnathdj56@gmail.com>
2025-11-25 11:16:11 +0530

    25/11/2025

---
+++ largest_3_same_digit_number_in_string.py
@@ -0,0 +1,6 @@
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        num_list = ['999', '888', '777', '666', '555', '444', '333', '222', '111', '000', ""]
+        for i in num_list:
+            if i in num:
+                return i
