7b8ab338064c5629d8949ff6a524f0989ff61761
Amarnathdj10 <amarnathdj56@gmail.com>
2026-05-27 20:31:35 +0530

    gotta restart

--- Data Structures and Algorithms/richest_customer_wealth.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        wealth = {}
-        for i in range(len(accounts)) :
-            wealth[i] = sum(accounts[i])
-        return max(wealth.values())
