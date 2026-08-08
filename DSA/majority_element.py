9e1ce8cfaa237264f704166a2cdd34536776f19d
Amarnathdj10 <amarnathdj56@gmail.com>
2026-08-09 00:11:26 +0530

    08/08/26

--- DSA/majority_element.py
+++ DSA/majority_element.py
@@ -1,14 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        map = {}
-        majority = 0
-        for i in nums:
-            if i not in map:
-                map[i] = 1
-            else:
-                map[i] += 1
-        for i in map:
-            if map[i] > n/2:
-                majority = map[i]
-                return i
+        res, count = 0, 0
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n==res else -1)
+        return res
